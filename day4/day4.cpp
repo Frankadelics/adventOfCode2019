7eec07a6eb2fdcbcd27a5f98bace23a34fac24ff
Francisco Arevalo <arevalofrancisco77@gmail.com>
2020-11-05 15:09:44 -0800

    Finished
    Day 4 parts 1 and 2 are done. I managed to translate the code to work with my bool idea and significantly increased the speed, at least it looked like it increases. Feels like it did. Either way, this code is significantly easier to read than the previous version because it's simpler. I changed the name of "holder" to "number" to better reflect what is being compared.

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -2,83 +2,75 @@
 #include <vector>
 #include <string>
 using namespace std;
-void printNumbers(vector<string>& num)
+
+void printNumbers(vector<string>& num) //For debugging purposes
 {
 	for (int i = 0; i < num.size(); i++)
 		cout << num[i] << endl;
 }
-void getNumbers(int start, int end, vector<string>& num)
+bool checkAdjacent(string number) //Check if number has any neighbors that are the same
+{
+	for (int i = 0; i < number.length() - 1; i++)
+	{
+		if (number[i] == number[i + 1])
+		{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+bool checkRepeatOnceAdjacent(string number) //Check if number has one pair of equal numbers only, no triplets, quadruplets, quintuplets 
+{
+	if (number[0] == number[1] && number[0] != number[2])
+		return true;
+	else if (number[1] == number[2] && number[1] != number[0] && number[1] != number[3])
+		return true;
+	else if (number[2] == number[3] && number[2] != number[1] && number[2] != number[4])
+		return true;
+	else if (number[3] == number[4] && number[3] != number[2] && number[3] != number[5])
+		return true;
+	else if (number[4] == number[5] && number[4] != number[3])
+		return true;
+	else
+		return false;
+}
+bool checkIncrease(string number) //Check if the numbers only increase
+{
+	bool check = true;
+	for (int i = 0; i < number.length() - 1; i++)
+	{
+		if (number[i] > number[i + 1])
+			check = false;
+	}
+	if (check)
+		return true;
+	else
+		return false;
+}
+void getNumbers(int start, int end, vector<string>& part1, vector<string>& part2)
 {
 	for (int i = start; i <= end; i++)
-		num.push_back(to_string(i));
-}
-void checkAdjacent(vector<string>& num, vector<string>& adj)
-{
-	string holder;
-	for (int i = 0; i < num.size(); i++)
 	{
-		holder = num[i];
-		for (int j = 0; j < num[i].length() - 1; j++)
+		string number = to_string(i);
+		if (checkAdjacent(number) && checkIncrease(number))
 		{
-			
-			if (holder[j] == holder[j + 1])
-			{
-				adj.push_back(holder);
-				break;
-			}
+			part1.push_back(number);
+			if (checkRepeatOnceAdjacent(number))
+				part2.push_back(number);
 		}
-	}
-}
-void checkRepeatOnceAdjacent(vector<string>& adj, vector<string>& repeatOnce)
-{
-	string holder;
-	for (int i = 0; i < adj.size(); i++)
-	{
-		holder = adj[i];
-		if (holder[0] == holder[1] && holder[0] != holder[2])
-			repeatOnce.push_back(holder);
-		else if (holder[1] == holder[2] && holder[1] != holder[0] && holder[1] != holder[3])
-			repeatOnce.push_back(holder);
-		else if (holder[2] == holder[3] && holder[2] != holder[1] && holder[2] != holder[4])
-			repeatOnce.push_back(holder);
-		else if (holder[3] == holder[4] && holder[3] != holder[2] && holder[3] != holder[5])
-			repeatOnce.push_back(holder);
-		else if (holder[4] == holder[5] && holder[4] != holder[3])
-			repeatOnce.push_back(holder);
-	}
-}
-void checkIncrease(vector<string>& num, vector<string>& inc)
-{
-	string holder;
-	bool check = false;
-	for (int i = 0; i < num.size(); i++)
-	{
-		holder = num[i];
-		check = true;
-		for (int j = 0; j < num[i].length() - 1; j++)
-		{
-			if (holder[j] > holder[j + 1])
-				check = false;
-		}
-		if (check)
-			inc.push_back(holder);
 	}
 }
 
 int main()
 {
-	vector<string> numbers;
-	vector<string> adjacentNumbers;
-	vector<string> increaseNumbers;
-	vector<string> repeatOnceAdjNumbers;
+	vector<string> numbersPart1;
+	vector<string> numbersPart2;
 
-	getNumbers(367479, 893698, numbers);
-	checkAdjacent(numbers, adjacentNumbers);
-	checkIncrease(adjacentNumbers, increaseNumbers);
-	checkRepeatOnceAdjacent(increaseNumbers, repeatOnceAdjNumbers);
+	getNumbers(367479, 893698, numbersPart1, numbersPart2);
 
-	cout << "Day 1: " << increaseNumbers.size() << endl;
-	cout << "Day 2: " << repeatOnceAdjNumbers.size() << endl;
+	cout << "Part 1: " << numbersPart1.size() << endl;
+	cout << "Part 2: " << numbersPart2.size() << endl;
 
 	return 0;
 }
