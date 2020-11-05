3ff9dfc26efce00fa8561cfc55dff5c85b38e819
Francisco Arevalo <arevalofrancisco77@gmail.com>
2020-11-05 13:46:38 -0800

    Day 4 Part 2 Finished
    Finished Day 4 part 2. This is how the code looke exactly when I got the solution...sorta. I am pushing this version so I can keep it in case I mess up. I am going to convert the functions I have to bool and see if that makes this thing a litte faster since I won't be looping through so much crap. Fingers crossed.

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -2,32 +2,49 @@
 #include <vector>
 #include <string>
 using namespace std;
-
-void getNumbers(int start, int end, vector<string>& num)
-{
-	for (int i = start; i <= end; i++)
-		num.push_back(to_string(i));
-}
 void printNumbers(vector<string>& num)
 {
 	for (int i = 0; i < num.size(); i++)
 		cout << num[i] << endl;
+}
+void getNumbers(int start, int end, vector<string>& num)
+{
+	for (int i = start; i <= end; i++)
+		num.push_back(to_string(i));
 }
 void checkAdjacent(vector<string>& num, vector<string>& adj)
 {
 	string holder;
 	for (int i = 0; i < num.size(); i++)
 	{
+		holder = num[i];
 		for (int j = 0; j < num[i].length() - 1; j++)
 		{
-			holder = num[i];
+			
 			if (holder[j] == holder[j + 1])
 			{
-				//cout << "True" << holder << endl;
 				adj.push_back(holder);
 				break;
 			}
 		}
+	}
+}
+void checkRepeatOnceAdjacent(vector<string>& adj, vector<string>& repeatOnce)
+{
+	string holder;
+	for (int i = 0; i < adj.size(); i++)
+	{
+		holder = adj[i];
+		if (holder[0] == holder[1] && holder[0] != holder[2])
+			repeatOnce.push_back(holder);
+		else if (holder[1] == holder[2] && holder[1] != holder[0] && holder[1] != holder[3])
+			repeatOnce.push_back(holder);
+		else if (holder[2] == holder[3] && holder[2] != holder[1] && holder[2] != holder[4])
+			repeatOnce.push_back(holder);
+		else if (holder[3] == holder[4] && holder[3] != holder[2] && holder[3] != holder[5])
+			repeatOnce.push_back(holder);
+		else if (holder[4] == holder[5] && holder[4] != holder[3])
+			repeatOnce.push_back(holder);
 	}
 }
 void checkIncrease(vector<string>& num, vector<string>& inc)
@@ -48,21 +65,20 @@ void checkIncrease(vector<string>& num, vector<string>& inc)
 	}
 }
 
-
-
 int main()
 {
 	vector<string> numbers;
 	vector<string> adjacentNumbers;
 	vector<string> increaseNumbers;
+	vector<string> repeatOnceAdjNumbers;
 
-	//getNumbers(111, 222, numbers);
 	getNumbers(367479, 893698, numbers);
-	//printNumbers(numbers);
 	checkAdjacent(numbers, adjacentNumbers);
 	checkIncrease(adjacentNumbers, increaseNumbers);
-	//printNumbers(increaseNumbers);
+	checkRepeatOnceAdjacent(increaseNumbers, repeatOnceAdjNumbers);
 
-	cout << "Size: " << increaseNumbers.size() << endl;
+	cout << "Day 1: " << increaseNumbers.size() << endl;
+	cout << "Day 2: " << repeatOnceAdjNumbers.size() << endl;
+
 	return 0;
 }
